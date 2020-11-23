8a57d9f6b37be2d1affd111eee19ebeaebc08b30
dinesh110598 <dineshramakrishnan2012@gmail.com>
2020-11-23 23:28:37 +0530

    Change hidden layers and made 1st elt arbitrary

--- library.py
+++ library.py
@@ -1,16 +1,7 @@
+# %%
 import tensorflow as tf
 import tensorflow.keras as tfk
-#import numpy as np
-
-class HiddenLayer (tfk.layers.Layer):
-    def __init__ (self, inshape, outshape, **kwargs):
-        super().__init__(**kwargs)
-        self.D = inshape
-        self.N_h = outshape
-
-    def call (self):
-        pass
-
+# %%
 class NADE (tfk.Model):
     def __init__(self, inshape, num_hidden, **kwargs):
         super().__init__(**kwargs)
@@ -18,52 +9,61 @@ class NADE (tfk.Model):
         self.D = inshape[0]*inshape[1]
         self.N_h = num_hidden
         self.flatten = tfk.layers.Flatten(input_shape=inshape)
-        self.HL = []
         self.output_layer = []
-        for _ in range(self.D):
-            self.HL.append(tfk.layers.Dense(self.N_h, 'sigmoid'))
+        for _ in range(self.D-1):
             self.output_layer.append (tfk.layers.Dense (1,'sigmoid'))
 
         #Implementing weight sharing between hidden layers
-        with tf.name_scope(self.HL[0].name):
-            self.HL[0].build(self.D-1)
-        for i in range (1,self.D-1):
-            with tf.name_scope(self.HL[i].name):
-                self.HL[i].build(self.D-i-1)
-            self.HL[i].kernel = self.HL[0].kernel[:,:(self.D-i-1)]
-            self.HL[i].bias = self.HL[0].bias
-            self.HL[i]._trainable_weights = []
-            self.HL[i]._trainable_weights.append(self.HL[i].kernel)
-            self.HL[i]._trainable_weights.append(self.HL[i].bias)
-
-        with tf.name_scope(self.HL[self.D-1].name):
-            self.HL[self.D-1].build(1)
-        self.HL[self.D-1].kernel = self.HL[0].kernel[:, :1]
-        self.HL[self.D-1].bias = self.HL[0].bias
-        self.HL[self.D-1]._trainable_weights = []
-        self.HL[self.D-1]._trainable_weights.append(self.HL[self.D-1].kernel)
-        self.HL[self.D-1]._trainable_weights.append(self.HL[self.D-1].bias)
+        rng = tf.random.Generator.from_non_deterministic_state ()
+        self.kernel = []
+        for _ in range (self.D-1):
+            self.kernel.append (tf.Variable (rng.uniform(shape=[self.N_h]), trainable=True))
+        self.bias = tf.Variable (tf.zeros (shape=(self.N_h,)), trainable=True)
+        self.loss_tracker = tfk.metrics.Mean(name="logit")
+    
+    def __SplDense (self, x):
+        kernel = tf.stack (self.kernel [:x.shape[...,0]])
+        return tfk.activations.sigmoid(tf.tensordot(x, kernel) + self.bias)
 
     def call (self, x):
+        """Calculates the probability of sample x
+        Args:
+            x (int32): Value of input lattice
+        """
         x = self.flatten (x)
-        h = []
-        for i in range (self.D-1):
-            h.append (self.HL[i] (x[:self.D-i-1]))
-        #Just make sure self.HL[self.D-1] receives only zero as input
-        h.append (self.HL[self.D-1](tf.constant (0)))
-        p = tf.zeros (self.D)
-        for i in range(self.D):
-            p[i] = (0.5*(1-x[i]))+x[i]*self.output_layer[i] (h[self.D-i-1])
-        return tf.reduce_prod (p) #This quantity is the joint probability of the input vector x
-
+        h = 0.
+        p = []
+        p.append (0.5) #The first lattice point is chosen arbitrarily
+        for i in reversed(range(self.D-1)):
+            h = self.__SplDense (x[:self.D-1-i])
+            p.append((0.5*(1-x[i]))+x[i]*self.output_layer[i](h))
+        return tf.reduce_prod (p, axis=-1) 
+        #Above quantity is the joint probability of the input vector x
 
     @tf.function
     def sample (self):
         x = []
-        prob = self.output_layer[0](self.HL[self.D-1](0))
-        x.append (1 if tf.random.uniform(shape=()) < prob else -1)
-        for i in reversed (range (self.D-1)):
+        rng = tf.random.Generator.from_non_deterministic_state()
+        prob = 0.5
+        x.append (1 if rng.uniform(shape=()) < prob else -1)
+        for i in reversed(range (self.D-1)):
             x_tensor = tf.constant (x)
-            prob = self.output_layer[self.D-1-i](self.HL[i](x_tensor))
+            prob = self.output_layer[i] (self.__SplDense (x_tensor))
             x.append(1 if tf.random.uniform(shape=()) < prob else -1)
         return tf.reshape (tf.constant (x), self.input_shape)
+    
+    @tf.function
+    def train_step (self, x):
+        with tf.GradientTape() as tape:
+            p = self.call (x)
+            loss = -tf.math.log (p)
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        # Update metrics (includes the metric that tracks the loss)
+        self.loss_tracker.update_state (loss)
+        # Return a dict mapping metric names to current value
+        return {"logits": self.loss_tracker.result()}
+# %%
