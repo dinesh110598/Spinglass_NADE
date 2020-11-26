0b10a4998a17d33c5a2f6f4b715bab03bfc389e3
dinesh110598 <dineshramakrishnan2012@gmail.com>
2020-11-26 16:58:59 +0530

    Minor Bug Fixes

--- library.py
+++ library.py
@@ -2,13 +2,13 @@
 import tensorflow as tf
 import tensorflow.keras as tfk
 # %%
-class NADE (tfk.Model):
+class NADE_orig (tfk.Model):
     def __init__(self, inshape, num_hidden, **kwargs):
         super().__init__(**kwargs)
-        self.input_shape = inshape
+        self.shape = inshape
         self.D = inshape[0]*inshape[1]
         self.N_h = num_hidden
-        self.flatten = tfk.layers.Flatten(input_shape=inshape)
+        self.flatten = tfk.layers.Flatten ()
         self.output_layer = []
         for _ in range(self.D-1):
             self.output_layer.append (tfk.layers.Dense (1,'sigmoid'))
@@ -17,53 +17,67 @@ class NADE (tfk.Model):
         rng = tf.random.Generator.from_non_deterministic_state ()
         self.kernel = []
         for _ in range (self.D-1):
-            self.kernel.append (tf.Variable (rng.uniform(shape=[self.N_h]), trainable=True))
-        self.bias = tf.Variable (tf.zeros (shape=(self.N_h,)), trainable=True)
-        self.loss_tracker = tfk.metrics.Mean(name="logit")
-    
-    def __SplDense (self, x):
-        kernel = tf.stack (self.kernel [:x.shape[...,0]])
-        return tfk.activations.sigmoid(tf.tensordot(x, kernel) + self.bias)
-
+            self.kernel.append (tf.Variable (rng.uniform(shape=[self.N_h]),
+                                trainable=True))
+        self.bias = tf.Variable (tf.zeros (shape=(self.N_h,)),
+                                trainable=True)
+        self.loss_tracker = tfk.metrics.Mean(name="logits")
     def call (self, x):
         """Calculates the probability of sample x
         Args:
             x (int32): Value of input lattice
         """
+
+        def SplDense(x, n):
+            """We are using this "layer" instead of regular keras Dense
+            layer to facilitate use of common kernel and bias"""
+            kernel = tf.stack(self.kernel[:n])
+            return tfk.activations.sigmoid(tf.matmul(x, kernel) + self.bias)
+        
         x = self.flatten (x)
         h = 0.
         p = []
-        p.append (0.5) #The first lattice point is chosen arbitrarily
-        for i in reversed(range(self.D-1)):
-            h = self.__SplDense (x[:self.D-1-i])
-            p.append((0.5*(1-x[i]))+x[i]*self.output_layer[i](h))
-        return tf.reduce_prod (p, axis=-1) 
+        p.append (tf.ones ([x.shape[0]])) #The first lattice point is fixed at +1
+        for i in range (1,self.D):
+            h = SplDense (x[:,:i], i)
+            y = tf.squeeze (self.output_layer[i-1] (h))
+            p.append (0.5*(1-x[:,i]) + x[:,i]*y)
+            #output_layer[i](h) produces the probabilty of x[i]=1
+        p = tf.stack (p, axis=-1)
+        return tf.reduce_prod (p, axis=-1)
         #Above quantity is the joint probability of the input vector x
+
+    @property
+    def metrics(self):
+        "Records the metrics that need to be refreshed at the end of every epoch"
+        return [self.loss_tracker]
 
     @tf.function
     def sample (self):
         x = []
         rng = tf.random.Generator.from_non_deterministic_state()
-        prob = 0.5
-        x.append (1 if rng.uniform(shape=()) < prob else -1)
+        prob = 1.
+        x.append (1.)
         for i in reversed(range (self.D-1)):
-            x_tensor = tf.constant (x)
+            x_tensor = tf.constant ([x])
             prob = self.output_layer[i] (self.__SplDense (x_tensor))
             x.append(1 if tf.random.uniform(shape=()) < prob else -1)
-        return tf.reshape (tf.constant (x), self.input_shape)
+        return tf.reshape (tf.constant (x), self.shape)
     
     @tf.function
     def train_step (self, x):
         with tf.GradientTape() as tape:
-            p = self.call (x)
+            p = self (x)
             loss = -tf.math.log (p)
         # Compute gradients
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
         # Update weights
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-        # Update metrics (includes the metric that tracks the loss)
+        # Updates loss metric by averaging current value with prev ones
         self.loss_tracker.update_state (loss)
+        print ("Traced")
         # Return a dict mapping metric names to current value
-        return {"logits": self.loss_tracker.result()}
+        return {"loss": self.loss_tracker.result()}
+
 # %%
