5f3b8c5d86c1ef513e9cf5b9189a6a1f67a381fa
dinesh110598 <dineshramakrishnan2012@gmail.com>
2020-12-01 15:17:02 +0530

    Created OutputLayer for GPU implementation

--- library.py
+++ library.py
@@ -85,3 +85,100 @@ class NADE_orig (tfk.Model):
         print ("Traced")
         return self.loss_tracker.result()
 # %%
+class OutputLayer (tfk.layers.Layer):
+    def __init__(self, D, N_h, **kwargs):
+        super().__init__(**kwargs)
+        rng = tf.random.Generator.from_non_deterministic_state()
+        self.kernel = tf.Variable (rng.uniform ([N_h, D-1]), trainable=True)
+        self.bias = tf.Variable (tf.zeros([D-1]), trainable=True)
+
+    def call (self, x):
+        return (tf.einsum ("ijk,kj->ij", x, self.kernel) + self.bias)
+
+class NADE_orig_GPU (tfk.Model):
+    def __init__(self, inshape, num_hidden, **kwargs):
+        super().__init__(**kwargs)
+        self.shape = inshape
+        self.D = inshape[0]*inshape[1]
+        self.N_h = num_hidden
+        self.flatten = tfk.layers.Flatten()
+        self.output_layer = []
+        for i in range(self.D-1):
+            self.output_layer.append(tfk.layers.Dense(1, 'sigmoid'))
+            self.output_layer[i].build([self.N_h])
+
+        #Implementing weight sharing between hidden layers
+        rng = tf.random.Generator.from_non_deterministic_state()
+        self.kernel = tf.Variable(rng.uniform(shape=[self.D-1,self.N_h]),
+                                trainable=True)
+        self.bias = tf.Variable(tf.zeros(shape=[self.N_h,]),
+                                trainable=True)
+        self.loss_tracker = tfk.metrics.Mean(name="logits")
+        self.optimizer = tfk.optimizers.SGD()
+
+    def call(self, x):
+        """Calculates the probability of sample x
+        Args:
+            x (int32): Value of input lattice
+        """
+
+        def SplDense(x, n):
+            """We are using this "layer" instead of regular keras Dense
+            layer to facilitate use of common kernel and bias"""
+            kernel = tf.stack(self.kernel[:n])
+            return tfk.activations.sigmoid(tf.matmul(x, kernel) + self.bias)
+
+        x = self.flatten(x)
+        p = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
+        Id = tf.ones([x.shape[0]])
+        p = p.write(0, Id)
+        #The first lattice point is fixed at +1
+        for i in range(1, self.D):
+            x1 = tf.gather(x, tf.range(i), axis=1)
+            h = SplDense(x1, i)
+            y = tf.squeeze(self.output_layer[i-1](h))
+            x2 = tf.gather(x, tf.constant(i), axis=1)
+            p = p.write(i, 0.5*(Id-x2) + (x2*y))
+        return tfm.reduce_mean(p.stack(), axis=0)
+        #Above quantity is the joint probability of the input vector x
+
+    def sample(self):  # Convert lists here to TensorArrays
+
+        def SplDense(x, n):
+            """We are using this "layer" instead of regular keras Dense
+            layer to facilitate use of common kernel and bias"""
+            kernel = tf.stack(self.kernel[:n])
+            return tfk.activations.sigmoid(tf.matmul(x, kernel) + self.bias)
+
+        x = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
+        rng = tf.random.Generator.from_non_deterministic_state()
+        prob = 1.
+        x = x.write(0, 1.)
+        for i in range(1, self.D):
+            prob = tf.squeeze(self.output_layer[i-1](SplDense(tf.expand_dims(x.stack(), axis=0),
+                                                              i)))
+            x = x.write(i, 1. if rng.uniform(shape=[]) < prob else -1.)
+        return tf.reshape(x.stack(), self.shape)
+
+    @tf.function
+    def train_step(self, x):
+        with tf.GradientTape() as tape:
+            p = self.call(x)
+            loss = -tf.math.log(p)
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        # Updates loss metric by averaging current value with prev ones
+        self.loss_tracker.update_state(loss)
+        print("Traced")
+        return self.loss_tracker.result()
+# %%
+g = tf.random.Generator.from_non_deterministic_state()
+x = g.uniform([20, 50, 40])
+ker = g.normal([40, 50])
+y = g.normal([20,20])
+print(tf.einsum("ijk,kj->ij",x,ker).shape)
+
+# %%
